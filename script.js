fd2b3a849831854ca24cb7f1d226802c74eac320
sanika <sanikagoyal9@gmail.com>
2022-10-10 17:29:38 +0530

    terrain added

--- script.js
+++ script.js
@@ -1,21 +1,80 @@
 let canvas = document.getElementById("canvas");
-canvas.width = 3500;
 canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
 
 let c = canvas.getContext("2d");
 
-function animate() {
-  c.fillStyle = "#6495ED";
-  c.fillRect(0, 0, canvas.width, canvas.height);
-  let img = new Image();
-  img.src = "images/track4.png";
+/*TRACK*/
 
-  img.addEventListener("load", () => {
-    for (i = 0; i < canvas.width; i++) c.drawImage(img, i, 520);
-  });
+c.fillStyle = "#6495ED";
+
+const trigno = (a, b, c) => {
+  return a + b + (a - b) * Math.cos(Math.PI * c);
+};
+let layers = [];
+
+function land(x) {
+  x = x / 200;
+  layers.push(Math.random() * 50);
+  return trigno(layers[Math.floor(x)], layers[Math.ceil(x)], x - Math.floor(x));
 }
-// 510 + 10 * Math.sin((Math.PI / 180) * i)
+
+/*CAR*/
+let img = new Image();
+
+let carMove = new (function () {
+  this.car = new Image();
+  this.car.src = "images/car.png";
+  this.x = canvas.width / 2 - 100;
+  this.y = 0;
+  this.ys = 0;
+  this.cdraw = function () {
+    let d = canvas.height - land(this.x + position);
+    if (d - 85 > this.y) this.ys += 0.1;
+    else {
+      this.y = d - 85;
+      //
+       this.ys = d - this.y - 90;
+      this.ys -= 0.1;
+    }
+    this.y += this.ys;
+    c.save();
+    c.translate(this.x, this.y);
+    c.drawImage(this.car, 10, 0, 150, 150);
+    c.restore();
+  };
+})();
+/*
+let update = function () {
+  window.addEventListener("keypress", (k) => {
+    switch (k.key) {
+      case "ArrowRight":
+        return 10;
+        break;
+    }
+  });
+*/
+let position = 0;
+function animate() {
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  carMove.cdraw();
+  /*
+  let x = 100;
+  for (let j = 0; j < 3; j++) {
+    let coin = new Image();
+    coin.src = "images/coin.png";
+    c.drawImage(coin, x, canvas.height - img.height, 100, 80);
+    x += 80;
+  }*/
+  img.src = "images/trackNew.jpeg";
+  position++;
+  for (let i = 0; i < canvas.width; i++)
+    c.drawImage(img, i, canvas.height - land(i + position));
+  requestAnimationFrame(animate);
+}
+
 animate();
+
 /*PETROL*/
 
 let petrol = document.getElementById("petrol");
@@ -41,22 +100,35 @@ petrolWork();
 
 /*CAR*/
 
-let car = document.getElementById("car");
+// let car = document.getElementById("car");
+// function carDown () {
+//   let carSpeed=5;
+//   let carP =+ carSpeed;
+//   // if(carP)
+//   car.style.top=carP +'px';
+// }
+// carDown();
 //let x = window.innerHeight;
-
+/*
 function carMove() {
   let carStart = 0;
   let x = 10;
   const CarMoving = setInterval(carAnimate, 100);
   function carAnimate() {
     carStart += x;
-    if (carStart > 2280) carStart = 0;
+    if (carStart > 900) carStart = 0;
     car.style.left = carStart + "px";
-    if (carStart > 200) {
+    if (carStart > 200 && carStart < 300) {
       car.style.left = carStart + "px";
-      //car.style.transform = "rotate(-45deg)";
+      car.style.bottom = carStart - 200 + "px";
     }
+    if (carStart > 300 && carStart < 400) {
+      car.style.left = carStart + "px";
+      car.style.top = carStart + "px";
+    }
+
     //if (carStart > 200 && carStart < 400) car.style.top = 0 + "px";
   }
 }
 carMove();
+*/
