ce9ac2106327e4667695c0dba7663407e1911d4e
sanika <sanikagoyal9@gmail.com>
2022-10-11 23:03:21 +0530

    first page linked + car + button added

--- javascript/script.js
+++ javascript/script.js
@@ -20,16 +20,27 @@ function land(x) {
 }
 
 /*CAR*/
+
 let img = new Image();
 
 let car = new Image();
-car.src = "images/car.png";
+car.src = "/images/car (2).png";
 let cX = 100;
-let cY = canvas.height - 200 - img.height;
+let cY = -100;
 let carMove = () => {
-  c.drawImage(car, cX, cY, 150, 150);
+  c.drawImage(car, cX, cY, 130, 150);
 };
-
+let d = document.getElementById("distance");
+function dCaclulate() {
+  let count = 0;
+  var p = 500;
+  count += cX;
+  distance.innerHTML = count;
+  // setInterval(() => {
+  //   distance.style.left = p + "px";
+  //   p+=1;
+  // }, 50);
+}
 window.addEventListener("keydown", (event) => {
   if (event.key == "ArrowRight") {
     cX += 10;
@@ -46,18 +57,19 @@ window.addEventListener("keydown", (event) => {
     cX += 5;
   }
 });
+
+/*ANIMATE FUNCTION */
 let position = 0;
 function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   carMove();
-  img.src = "images/trackNew.jpeg";
-  // position += 1;
+  dCaclulate();
+  img.src = "/images/trackNew.jpeg";
+  position += 1;
   for (let i = 0; i < canvas.width; i++)
-    c.drawImage(img, i, canvas.height - land(i) - 40);
+    c.drawImage(img, i, canvas.height - land(i + position) - 40);
   requestAnimationFrame(animate);
 }
-
-animate();
 
 /*PETROL*/
 
@@ -65,7 +77,6 @@ let petrol = document.getElementById("petrol");
 let pLine = document.getElementById("petrol-line");
 
 let start = 230;
-
 let PBar = setInterval(petrolWork, 500);
 function petrolWork() {
   if (start < 0) clearInterval(PBar, 500);
@@ -81,17 +92,23 @@ function petrolWork() {
   pLine.style.width = start + "px";
 }
 petrolWork();
-
-/*COINS
-let coin = new Image();
-coin.src = "images/coin.png";
-c.drawImage(coin, 100, 100);
-/*
-let coinDistance = 500;
-for (let j = 0; j < 5; j++) {
-  let coin = new Image();
-  coin.src = "images/coin.png";
-  c.drawImage(coin, coinDistance, canvas.height - land(j - 15), 100, 100);
-  coinDistance += 70;
+/*start*/
+let bar = document.getElementById("line");
+let load = document.getElementById("click-me");
+function startShow() {
+  load.style.visibility = "hidden";
 }
-*/
+
+load.addEventListener("click", () => {
+  const cDown = setInterval(carDown, 50);
+  function carDown() {
+    cY += 15;
+    if (cY > canvas.height - 80 - img.height) clearInterval(cDown);
+    setTimeout(startShow(), 2000);
+  }
+  animate();
+  petrol.style.visibility = "visible";
+  pLine.style.visibility = "visible";
+  d.style.visibility = "visible";
+  bar.style.visibility = "visible";
+});
